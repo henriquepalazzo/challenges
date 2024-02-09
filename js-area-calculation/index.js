088226e6f69b16f996b944ddc21d9e388d7ffed2
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-09 16:53:57 +0100

    Add logic for the calculation of the area and circunference.

---
+++ js-area-calculation/index.js
@@ -0,0 +1,6 @@
+let circleRadius = 5;
+let circleArea = Math.PI * Math.pow(circleRadius, 2);
+let circunference = 2 * Math.PI * circleRadius;
+console.log(`For a circle with a radius of ${circleRadius} units:`);
+console.log(`The area is approximately ${circleArea} square units.`);
+console.log(`The circumference is approximately ${circunference} units.`);
