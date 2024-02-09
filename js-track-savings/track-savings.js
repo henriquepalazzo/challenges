d8552a72a77cfc4305507c50e7887d929e0b8591
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-09 16:16:42 +0100

    Add logic for calculating the savings acumulated.

---
+++ js-track-savings/track-savings.js
@@ -0,0 +1,7 @@
+const startingBalance = 1000;
+const monthlyContribution = 350;
+const months = 36;
+
+const totalSavings = startingBalance + (monthlyContribution*months);
+
+console.log(`After ${months} months of saving, you will have ${totalSavings} in your account.`);
