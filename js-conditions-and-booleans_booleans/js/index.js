053dcdbc547000ad046ce26134eaabfd78fa7a51
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-12 15:25:05 +0100

    Add many Boolean tests to the index.js.

--- js-conditions-and-booleans_booleans/js/index.js
+++ js-conditions-and-booleans_booleans/js/index.js
@@ -1,3 +1,16 @@
 console.clear();
 
 console.log(Boolean("Try it out"));
+
+console.log('2 == "2": ', 2 == "2");
+console.log('2 === "2": ', 2 === "2");
+console.log('2 === 2: ', 2 === 2);
+console.log('"Hello world": ', Boolean("Hello world"));
+console.log('"": ', Boolean(""));
+console.log('true: ', Boolean(true));
+console.log('0: ', Boolean(0));
+console.log('-50: ', Boolean(-50));
+console.log('50: ', Boolean(50));
+console.log('5 > 1: ', Boolean(5 > 1));
+console.log('"1 > 10"', Boolean("1 > 10"));
+console.log('"false"', Boolean("false"));
