1ed21f4e2f4fcfcc9b7eaaed6d262389c5263458
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-12 11:47:50 +0100

    Add functionality to the application.

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -6,3 +6,16 @@ const buttonDarkMode = document.querySelector('[data-js="dark-mode-button"]');
 const buttonLightMode = document.querySelector('[data-js="light-mode-button"]');
 const buttonToggle = document.querySelector('[data-js="toggle-button"]');
 
+buttonDarkMode.addEventListener('click', () => {
+    bodyElement.classList.add('dark')
+    bodyElement.classList.remove('light')
+})
+
+buttonLightMode.addEventListener('click', () => {
+    bodyElement.classList.add('light')
+    bodyElement.classList.remove('dark')
+})
+
+buttonToggle.addEventListener('click', () => {
+    bodyElement.classList.toggle('dark')
+})
