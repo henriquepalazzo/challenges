2f5398140d867f97524d0327510634a508652dfd
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-12 11:44:11 +0100

    Select the three buttons on the js.

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -1,3 +1,8 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const buttonDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+const buttonLightMode = document.querySelector('[data-js="light-mode-button"]');
+const buttonToggle = document.querySelector('[data-js="toggle-button"]');
+
