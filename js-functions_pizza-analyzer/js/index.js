2812479eb9601c41b8c3a8b8c2931acaa9af3b2f
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-13 16:16:32 +0100

    Add functions to calculate the pizza area

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,15 +9,31 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value
+  const pizzaSize2 = pizzaInput2.value
+  calculatePizzaGain(pizzaSize1, pizzaSize2)
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value
+  const pizzaSize2 = pizzaInput2.value
+  calculatePizzaGain(pizzaSize1, pizzaSize2)
 });
 
 // Task 1
 // define the function calculatePizzaGain here
+
+function calculatePizzaGain(diameter1, diameter2) {
+  const area1 = calculatePizzaArea(diameter1)
+  const area2 = calculatePizzaArea(diameter2)
+  const pizzaGain = (area2 - area1) / area1 * 100
+  output.textContent = Math.round(pizzaGain)
+}
+
+function calculatePizzaArea(diameter) {
+  const radius = diameter/2
+  return Math.PI*(radius*radius)
+}
 
 // Task 2
 // define the function updatePizzaDisplay here
