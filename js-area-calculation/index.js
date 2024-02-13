3feabf02c22f5055954100decaeec545598cfbad
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-13 10:51:19 +0100

    Improve the display of values showing only two decimals after the comma.

--- js-area-calculation/index.js
+++ js-area-calculation/index.js
@@ -1,6 +1,6 @@
 let circleRadius = 5;
 let circleArea = Math.PI * Math.pow(circleRadius, 2);
 let circunference = 2 * Math.PI * circleRadius;
-console.log(`For a circle with a radius of ${circleRadius} units:`);
-console.log(`The area is approximately ${circleArea} square units.`);
-console.log(`The circumference is approximately ${circunference} units.`);
+console.log(`For a circle with a radius of ${circleRadius.toFixed(2)} units:`);
+console.log(`The area is approximately ${circleArea.toFixed(2)} square units.`);
+console.log(`The circumference is approximately ${circunference.toFixed(2)} units.`);
