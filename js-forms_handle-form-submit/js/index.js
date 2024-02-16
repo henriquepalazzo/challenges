c551fa2846befa2f33b4926176d3a5b0eb05fc74
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-16 11:16:43 +0100

    Add logic to calculate and log the age-badness index.

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -8,6 +8,9 @@ form.addEventListener('submit', (event) => {
     const data = Object.fromEntries(formData)
     console.log(data);
 
+    const ageBadness = Number(data.age) + Number(data.badness)
+    console.log(`The age-badness-sum of ${data.firstName} is ${ageBadness}`);
+
     event.target.elements.firstName.focus();
     event.target.reset();
 })
