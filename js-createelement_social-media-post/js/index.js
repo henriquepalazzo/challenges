1c6d8ca5bd97e2bb82f8dd68074e4aa052b4aa22
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-16 16:43:05 +0100

    Add copy of element using the createElement method.

--- js-createelement_social-media-post/js/index.js
+++ js-createelement_social-media-post/js/index.js
@@ -10,3 +10,27 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const body = document.querySelector("body")
+const post = document.createElement("section")
+const postContent = document.createElement("p")
+const postFooter = document.createElement("footer")
+const username = document.createElement("span")
+const button = document.createElement("post__button")
+
+post.classList.add("post")
+postContent.classList.add("post__content")
+postContent.innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+postFooter.classList.add("post__footer")
+username.classList.add("post__username")
+username.innerHTML = "@username"
+button.innerHTML = "♥ Like"
+button.classList.add("post__button")
+postFooter.append(username)
+postFooter.append(button)
+
+post.append(postContent)
+post.append(postFooter)
+body.append(post)
+
+button.addEventListener('click', handleLikeButtonClick)
