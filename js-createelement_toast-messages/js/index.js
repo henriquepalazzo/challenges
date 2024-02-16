dff5da161532f4cb928d9c5d714fe610cafa53ae
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-16 17:08:13 +0100

    Add logic to create new toast messages and delete all the messages.

--- js-createelement_toast-messages/js/index.js
+++ js-createelement_toast-messages/js/index.js
@@ -5,9 +5,12 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  const message = document.createElement("li")
+  message.classList.add("toast-container__message")
+  message.innerHTML = "I'm a toast message."
+  toastContainer.append(message)
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = ""
 });
