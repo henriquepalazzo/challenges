3a0f9dc7b75cfd0b89a6d72ef31b82529b720136
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-21 13:42:09 +0100

    Add logic to calculate the double of each number in a given array.

--- js-callback-calculate-total-price/index.js
+++ js-callback-calculate-total-price/index.js
@@ -20,3 +20,18 @@ function extractPrice(product) {
     return product.price
 }
 console.log('The total price is: '+calculateTotalPrice(products));
+
+function myMap(numbers, double) {
+    const results = []
+    for (const number of numbers) {
+        results.push(double(number))
+    }
+    return results
+}
+function double(number) {
+    return number * 2
+}
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(myMap(numbers, double))
