f3dd06079f749ec7c8001e13d5d9ce62dce3f997
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-21 11:29:46 +0100

    Add logic to calculate the total price based on a given array of products objects.

---
+++ js-callback-calculate-total-price/index.js
@@ -0,0 +1,22 @@
+const products = [
+    { name: "Saffron", price: 50 },
+    { name: "Cinnamon", price: 6 },
+    { name: "Cardamom", price: 12 },
+    { name: "Turmeric", price: 9 },
+    { name: "Paprika", price: 4 },
+    { name: "Garam Masala", price: 10 },
+    { name: "Vanilla Bean", price: 15 },
+    { name: "Coriander", price: 6 },
+];
+
+function calculateTotalPrice(products, extractPrices) {
+    let totalPrice = 0
+    for (const product of products) {
+        totalPrice += extractPrice(product)
+    }
+    return totalPrice
+}
+function extractPrice(product) {
+    return product.price
+}
+console.log('The total price is: '+calculateTotalPrice(products));
