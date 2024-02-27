10836c1344ed8f74bc2e36200286b00260bdad1a
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-27 11:18:04 +0100

    Add logic to show cards based on the data fetched from the API.

--- js-fetch_star-wars/js/index.js
+++ js-fetch_star-wars/js/index.js
@@ -39,8 +39,11 @@ renderElement(firstCard);
 
 fetchDataAndRender();
 
-// --v-- your code below this line --v--
-
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.dev/api/people");
+  const data = await response.json();
+  data.results.forEach((cardContent) => {
+    const card = Card(cardContent);
+    renderElement(card);
+  });
 }
