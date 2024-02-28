8bee388bb21a93a47b1601c6f6ebc0b5a010978c
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-28 13:45:49 +0100

    Add method to aclompish the test for the method getAnimal.

--- js-unit-testing_get-animal/index.js
+++ js-unit-testing_get-animal/index.js
@@ -1,1 +1,8 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  const responses = {
+    cats: "I totally love cats!",
+    dogs: "I like dogs!",
+    default: "I do not like animals at all!",
+  };
+  return responses[animal] || responses.default;
+}
