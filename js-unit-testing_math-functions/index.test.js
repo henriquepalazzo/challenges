b3bb3eb2332d952bac93938c1ed4386137c1c946
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-29 18:13:40 +0100

    Add test logic for the add method.

---
+++ js-unit-testing_math-functions/index.test.js
@@ -0,0 +1,14 @@
+import { add } from "./index.js";
+
+test("returns 5 if called with add(2, 3)", () => {
+  const result = add(2, 3);
+  expect(result).toBe(5);
+});
+test("returns a negative value if the greater argument is negative", () => {
+  const result = add(1, -4);
+  expect(result).toBeLessThan(0);
+});
+test("returns a value close to 0.3 if called with add(0.1, 0.2)", () => {
+  const result = add(0.1, 0.2);
+  expect(result).toBeCloseTo(0.3);
+});
