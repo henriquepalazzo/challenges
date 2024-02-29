a2dc90eaa6ac0dab1256117392c096a98ca858ae
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-02-29 18:17:11 +0100

    Add test logic for the multiply method.

--- js-unit-testing_math-functions/index.test.js
+++ js-unit-testing_math-functions/index.test.js
@@ -1,4 +1,4 @@
-import { add, subtract } from "./index.js";
+import { add, multiply, subtract } from "./index.js";
 
 test("returns 5 if called with add(2, 3)", () => {
   const result = add(2, 3);
@@ -21,4 +21,20 @@ test("returns 10 if called with subtract(15, 5)", () => {
 test("returns a negative value if the second argument is greater than the first one", () => {
   const result = subtract(2, 5);
   expect(result).toBeLessThan(0);
+});
+test("returns 8 if called with multiply(2, 4)", () => {
+  const result = multiply(2, 4);
+  expect(result).toBe(8);
+});
+test("returns a negative value if only the first argument is negative", () => {
+  const result = multiply(-2, 3);
+  expect(result).toBeLessThan(0);
+});
+test("returns a negative value if only the second argument is negative", () => {
+  const result = multiply(2, -3);
+  expect(result).toBeLessThan(0);
+});
+test("returns a positive value if called with two negative arguments", () => {
+  const result = multiply(-2, -2);
+  expect(result).toBeGreaterThan(0);
 });
