c6e9e2d256d08d427c5f7fc21ab08abdaed3a617
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-05 11:42:34 +0100

    Add basic button component.

--- react-props_button/src/App.jsx
+++ react-props_button/src/App.jsx
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Button color="#ffcc00" text="Click here NOW!" />
+      <Button color="#ff0000" text="Don't click me!" disabled />
+    </>
+  );
+}
+
+function Button({ color, disabled, text }) {
+  return (
+    <button
+      style={{
+        backgroundColor: color,
+        border: 0,
+        padding: 20,
+        borderRadius: 5,
+        fontSize: 20,
+      }}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 }
