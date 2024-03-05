7f7b2085438b5a30f5e359e4309a3e9a062b8bd8
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-05 10:58:10 +0100

    Add component sum.

--- react-props_sum/src/App.jsx
+++ react-props_sum/src/App.jsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={3} valueB={3} />;
+}
+
+function Sum({ valueA, valueB }) {
+  const sum = valueA + valueB;
+  return (
+    <h1>
+      The result of the sum of {valueA} and {valueB} is: {sum}
+    </h1>
+  );
 }
