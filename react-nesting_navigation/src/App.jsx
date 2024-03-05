e159275db1087fc0c151f2838b34a5d8e3763bad
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-05 18:48:30 +0100

    Add Avatar component.

--- react-nesting_navigation/src/App.jsx
+++ react-nesting_navigation/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
-import avatar from "./assets/avatar.jpg";
 import Link from "./components/Link";
 import Image from "./components/Image";
 import Logo from "./components/Logo";
+import Avatar from "./components/Avatar";
 
 export default function App() {
   return (
@@ -16,13 +16,7 @@ export default function App() {
           <Link href="#about">About</Link>
           <Link href="#impressum">Impressum</Link>
         </Navigation>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <Image src={avatar} alt="avatar" />
-        </button>
+        <Avatar />
       </Header>
       <main>content goes here…</main>
     </>
