494c7ceaa326d98f44b602b04987ec8b2ddad214
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-05 18:46:35 +0100

    Add Logo component.

--- react-nesting_navigation/src/App.jsx
+++ react-nesting_navigation/src/App.jsx
@@ -2,17 +2,15 @@ import "./App.css";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import avatar from "./assets/avatar.jpg";
-import logo from "./assets/logo.jpg";
 import Link from "./components/Link";
 import Image from "./components/Image";
+import Logo from "./components/Logo";
 
 export default function App() {
   return (
     <>
       <Header>
-        <Link href="#">
-          <Image src={logo} alt="logo" />
-        </Link>
+        <Logo />
         <Navigation>
           <Link href="#home">Home</Link>
           <Link href="#about">About</Link>
