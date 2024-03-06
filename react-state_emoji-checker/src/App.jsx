c40c19c49cd1353213f57aafcece87a0456dd415
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-06 16:10:28 +0100

    Add logic to implement useState method to control the variable code.

--- react-state_emoji-checker/src/App.jsx
+++ react-state_emoji-checker/src/App.jsx
@@ -1,51 +1,34 @@
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
-  let code = "?";
+  const [code, setCode] = useState("");
+  const validCode = "🐡🐡🐠🐋🐋";
 
-  const validCode = "🐡🐠🐋";
-
+  function handleClick(emoji) {
+    setCode(code + emoji);
+  }
   return (
     <div className="container">
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={() => handleClick("🐡")}>
           <span role="img" aria-label="Pufferfish">
             🐡
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={() => handleClick("🐋")}>
           <span role="img" aria-label="Whale">
             🐋
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={() => handleClick("🐠")}>
           <span role="img" aria-label="Clownfish">
             🐠
           </span>
         </button>{" "}
       </div>
 
-      <button
-        type="button"
-        onClick={() => {
-          console.log("Reset Code!");
-        }}
-      >
+      <button type="button" onClick={() => setCode("")}>
         Reset
       </button>
       <h2>{code}</h2>
