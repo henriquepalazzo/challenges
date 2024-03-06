be64a20701769700f04008c0bcac3c86a5481ff5
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-06 15:43:21 +0100

    Add useState method to control the count variable.

--- react-state_counter/src/App.jsx
+++ react-state_counter/src/App.jsx
@@ -1,26 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={() => setCount(count - 1)}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={() => setCount(count + 1)}>
           +
         </button>
       </div>
