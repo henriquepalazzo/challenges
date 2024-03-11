3523778121041c3906155b8e17fa1656204125db
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-11 13:40:47 +0100

    Add logic to handle the Form component data on the App component and update the fields.

--- react-state-2_personal-details/src/App.jsx
+++ react-state-2_personal-details/src/App.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+
 import "./App.css";
 import Form from "./components/Form.jsx";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleCreateUser(event) {
+    event.preventDefault();
+    const formFields = event.target.elements;
+    setName(formFields.name.value);
+    setEmail(formFields.email.value);
+    event.target.reset();
+  }
+
   return (
     <div className="container">
       <h1>Personal Details Form</h1>
-      <Form />
+      <Form onSubmit={handleCreateUser} />
       <h2>Your submitted details:</h2>
       <p>
-        Name: <span className="output">John doe</span>
+        Name: <span className="output">{name}</span>
       </p>
       <p>
-        Email: <span className="output">John@Doe.com</span>
+        Email: <span className="output">{email}</span>
       </p>
     </div>
   );
