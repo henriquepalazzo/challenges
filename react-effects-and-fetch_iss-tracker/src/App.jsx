f25b55bab84478b16f6101347a9c0f58ed5b0d16
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-12 14:26:10 +0100

    Add logic to fetch the ISS coordinates every 5 seconds.

--- react-effects-and-fetch_iss-tracker/src/App.jsx
+++ react-effects-and-fetch_iss-tracker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./App.css";
@@ -11,7 +11,20 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setCoords({
+      longitude: data.longitude,
+      latitude: data.latitude,
+    });
+  }
+
+  useEffect(() => {
+    getISSCoords();
+    const intervalId = setInterval(getISSCoords, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <main>
