2306257fa3b3f1e08b740c49f537dab52116668c
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-20 15:29:28 +0100

    Add test renders a label and an input with the correct attributes

--- react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -3,8 +3,24 @@ import userEvent from "@testing-library/user-event";
 import Input from ".";
 
 test("renders a label and an input with the correct attributes", () => {
-  render(<Input></Input>);
-  const input = screen.get;
+  render(
+    <Input
+      name={"Testing"}
+      labelText="Testing label"
+      placeholder={"Write here"}
+      type={"text"}
+      value={"Some value"}
+      required={"required"}
+    ></Input>
+  );
+  const input = screen.getByRole("textbox");
+
+  expect(input).toHaveAttribute("id");
+  expect(input).toHaveAttribute("name", "Testing");
+  expect(input).toHaveAttribute("placeholder", "Write here");
+  expect(input).toHaveAttribute("type", "text");
+  expect(input).toHaveAttribute("value", "Some value");
+  expect(input).toHaveAttribute("required");
 });
 
 test("calls callback on every user input", async () => {
