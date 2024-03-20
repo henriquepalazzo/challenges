da0de72662bdccd40ee9fe288fe82299992cdd55
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-20 16:06:21 +0100

    Add test calls callbacks when increasing or decreasing score

--- react-component-testing_scorekeeper/components/Player/Player.test.js
+++ react-component-testing_scorekeeper/components/Player/Player.test.js
@@ -10,4 +10,29 @@ test("renders player information and two buttons", () => {
   expect(buttons).toHaveLength(2);
 });
 
-test("calls callbacks when increasing or decreasing score", async () => {});
+test("calls callbacks when increasing or decreasing score", async () => {
+  const handleIncreaseScore = jest.fn();
+  const handleDecreaseScore = jest.fn();
+
+  render(
+    <Player
+      onIncreasePlayerScore={handleIncreaseScore}
+      onDecreasePlayerScore={handleDecreaseScore}
+    ></Player>
+  );
+
+  const buttonIncreaseScore = screen.getByRole("button", {
+    name: "Increase Score",
+  });
+  const buttonDecreaseScore = screen.getByRole("button", {
+    name: "Decrease Score",
+  });
+
+  const user = userEvent.setup();
+
+  await user.click(buttonIncreaseScore);
+  await user.click(buttonDecreaseScore);
+
+  expect(handleIncreaseScore).toHaveBeenCalled();
+  expect(handleDecreaseScore).toHaveBeenCalled();
+});
