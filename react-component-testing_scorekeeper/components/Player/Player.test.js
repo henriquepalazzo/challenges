a782e5703febdee5c522625d548cd0661f878ccc
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-20 15:52:06 +0100

    Add test renders player information and two buttons

--- react-component-testing_scorekeeper/components/Player/Player.test.js
+++ react-component-testing_scorekeeper/components/Player/Player.test.js
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Player from ".";
 
-test("renders player information and two buttons", () => {});
+test("renders player information and two buttons", () => {
+  render(<Player name="Jacinto Gloria"></Player>);
+  const playerInformation = screen.getByText(/Jacinto Gloria/i);
+  const buttons = screen.getAllByRole("button");
+  expect(playerInformation).toBeInTheDocument();
+  expect(buttons).toHaveLength(2);
+});
 
 test("calls callbacks when increasing or decreasing score", async () => {});
