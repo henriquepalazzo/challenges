683a521f0321c0ba3d88ea1d39df3e81d5fe7e2d
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-20 14:15:49 +0100

    Add test calls callback on every user input

--- react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,17 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input></Input>);
+  const input = screen.get;
+});
 
-test("calls callback on every user input", async () => {});
+test("calls callback on every user input", async () => {
+  const onChange = jest.fn();
+  const user = userEvent.setup();
+
+  render(<Input onChange={onChange}></Input>);
+  const input = screen.getByRole("textbox");
+  await user.type(input, "Anything");
+  expect(onChange).toHaveBeenCalledTimes(8);
+});
