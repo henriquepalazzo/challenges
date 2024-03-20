103b54a355d7f1d67eba47ae3bc39dfeaee3d099
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-20 13:35:42 +0100

    Add test submits the correct form data when every field is filled out

--- react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
+++ react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import GameForm from "./index";
 
@@ -26,6 +26,27 @@ test("renders a form with the accessible name 'Create a new game'", () => {
   expect(name).toBeInTheDocument();
 });
 
-test("submits the correct form data when every field is filled out", async () => {});
+test("submits the correct form data when every field is filled out", async () => {
+  const mockCreateGame = jest.fn();
+  render(<GameForm onCreateGame={mockCreateGame}></GameForm>);
+  const user = userEvent.setup();
+  const inputGameName = screen.getByLabelText(/Name of game/i);
+  expect(inputGameName).toBeInTheDocument();
+  const inputPlayersName = screen.getByLabelText(
+    /Player names, separated by comma/i
+  );
+  expect(inputPlayersName).toBeInTheDocument();
+  await user.type(inputGameName, "Backgamon");
+  await user.type(inputPlayersName, "Marcos Kraus");
+  const button = screen.getByRole("button", { name: /Create game/i });
+  await user.click(button);
+
+  await waitFor(() => {
+    expect(mockCreateGame).toHaveBeenCalledWith({
+      nameOfGame: "Backgamon",
+      playerNames: ["Marcos Kraus"],
+    });
+  });
+});
 
 test("does not submit form if one input field is left empty", async () => {});
