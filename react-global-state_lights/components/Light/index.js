94608d1c5671b613c9f731f128045392fc6359b5
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-21 11:36:24 +0100

    Lift up the state and refactor the code to work with global state.

--- react-global-state_lights/components/Light/index.js
+++ react-global-state_lights/components/Light/index.js
@@ -1,21 +1,9 @@
 import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
-export default function Light({ name }) {
-  const [isOn, setIsOn] = useState(false);
-
-  function handleToggle() {
-    setIsOn((isOn) => !isOn);
-  }
-
+export default function Light({ id, name, isOn, toggleLight }) {
   return (
-    <LightButton
-      type="button"
-      onClick={() => {
-        handleToggle();
-      }}
-      $isOn={isOn}
-    >
+    <LightButton type="button" onClick={() => toggleLight(id)} $isOn={isOn}>
       <Icon $isOn={isOn}>💡</Icon>
       <Text>
         <Name>{name}</Name>
