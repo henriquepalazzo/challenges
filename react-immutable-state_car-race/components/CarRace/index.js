5eea2037082f247fa00530542e3e2b03d793e3ce
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-22 11:40:03 +0100

    Add logic to the move function with useImmer library.

--- react-immutable-state_car-race/components/CarRace/index.js
+++ react-immutable-state_car-race/components/CarRace/index.js
@@ -8,35 +8,27 @@ import {
   Track,
 } from "./CarRace.styled";
 import { initialCars, getRandomDistance } from "../../utils/utils";
+import { useImmer } from "use-immer";
 
 const finishLine = 200;
 
 export default function CarRace() {
-  const [cars, setCars] = useState(initialCars);
+  const [cars, updateCars] = useImmer(initialCars);
 
   function moveCar(clickedCar) {
     const coveredDistance = getRandomDistance();
-    setCars(
-      cars.map((car) =>
-        car.emoji !== clickedCar.emoji
-          ? car
-          : {
-              ...car,
-              position: {
-                x: car.position.x + coveredDistance,
-                lastDistance: coveredDistance,
-              },
-            }
-      )
-    );
+    updateCars((draft) => {
+      const car = draft.find((car) => car.emoji === clickedCar.emoji);
+      car.position.x = car.position.x + coveredDistance;
+      car.position.lastDistance = coveredDistance;
+    });
   }
-
   const winner = cars.find((car) => car.position.x >= finishLine);
 
   return (
     <>
       {winner ? (
-        <Winner winner={winner} onRestart={() => setCars(initialCars)} />
+        <Winner winner={winner} onRestart={() => updateCars(initialCars)} />
       ) : (
         <AllCarRoutes $finishLine={finishLine}>
           <DistanceHeadline>Last Distance</DistanceHeadline>
