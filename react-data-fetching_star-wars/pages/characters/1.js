a7556884f62f936856599d12ee1ae5d695d76814
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-24 19:22:20 +0100

    Load data using SWR.

--- react-data-fetching_star-wars/pages/characters/1.js
+++ react-data-fetching_star-wars/pages/characters/1.js
@@ -1,17 +1,40 @@
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import useSWR from "swr";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = new Error("An error ocurred while loading the data");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+};
 
 export default function Character() {
   const id = 1;
+  const { data, error, isLoading } = useSWR(
+    `https://swapi.dev/api/people/${id}`,
+    fetcher
+  );
+
+  if (isLoading) {
+    return <h1>Loading data...</h1>;
+  }
+  if (error) {
+    return <h1>Error loading data...</h1>;
+  }
 
   return (
     <Layout>
       <Card
         id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
       />
     </Layout>
   );
